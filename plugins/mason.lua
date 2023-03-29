290400004fa0a70f105076938aa8406e4c22f322
Agung Baptiso Sorlawan <tisorlawan@gmail.com>
2023-03-30 00:30:25 +0700

    fix: add prismals

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     opts = function(_, opts)
       if not opts.ensure_installed then opts.ensure_installed = {} end
-      utils.list_insert_unique(opts.ensure_installed, { "ruff_lsp", "eslint" })
+      utils.list_insert_unique(opts.ensure_installed, { "ruff_lsp", "eslint", "prismals" })
     end,
   },
 }
