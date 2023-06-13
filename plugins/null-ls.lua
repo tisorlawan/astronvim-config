7d8e70ef3693d2a6c6a7966e74f5bc7c134595d8
Agung Baptiso Sorlawan <tisorlawan@gmail.com>
2023-06-13 18:16:10 +0700

    fix

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -1,16 +1,16 @@
-local nls = require "null-ls"
-
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  opts = function(_, opts)
-    opts.debug = true
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      opts.debug = true
 
-    local sources = {}
+      local sources = {}
 
-    if type(opts.sources) == "table" then
-      vim.list_extend(opts.sources, sources)
-    else
-      opts.sources = sources
-    end
-  end,
+      if type(opts.sources) == "table" then
+        vim.list_extend(opts.sources, sources)
+      else
+        opts.sources = sources
+      end
+    end,
+  },
 }
